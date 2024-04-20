40bb7a92a45a3fb8fecdd02995fe7805c581f4fc
jawabreh0 <ahmadjawabreh@protonmail.com>
2024-04-20 11:58:21 +0300

    index.py commit

---
+++ index.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+import uvicorn
+from cmnd.get_tools import get_tools
+from cmnd.run_tool import post_run_tool
+
+app = FastAPI()
+
+@app.get("/cmnd-tools")
+async def read_cmnd_tools():
+    return get_tools()
+
+@app.post("/run-cmnd-tool")
+async def run_cmnd_tool(tool_name: str, props: dict):
+    return post_run_tool(tool_name, props)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
