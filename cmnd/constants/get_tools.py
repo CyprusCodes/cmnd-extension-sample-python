e5d4648162a9750bcd776148883cff9212f7ec73
jawabreh0 <ahmadjawabreh@protonmail.com>
2024-04-20 12:00:37 +0300

    get_tools.py commit

---
+++ cmnd/constants/get_tools.py
@@ -0,0 +1,13 @@
+from .tools import tools
+
+def get_tools():
+    tools_mapped = [{'name': tool.name,
+                     'description': tool.description,
+                     'jsonSchema': tool.parameters,
+                     'isDangerous': tool.dangerous,
+                     'functionType': tool.functionType,
+                     'isLongRunningTool': tool.isLongRunningTool,
+                     'rerun': tool.rerun,
+                     'rerunWithDifferentParameters': tool.rerunWithDifferentParameters}
+                    for tool in tools]
+    return {'tools': tools_mapped}
